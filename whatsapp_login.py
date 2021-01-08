fcb6d6547e98b70e914df7aa61fea4ef545c7119
Lucas Peixoto <lucas.sacramoni@yahoo.com>
2021-01-08 09:29:30 -0300

    Inclusão de Condicionais no whatsapp_login

--- whatsapp_login.py
+++ whatsapp_login.py
@@ -3,19 +3,43 @@
 
 from selenium import webdriver 
 from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.support import expected_conditions as EC
+
 import tkinter.messagebox
+import time
 
 def whatsapp_login(self):
+    
 
     link = "https://web.whatsapp.com/"
 
-    self.driver = webdriver.Chrome(self.path_chromedriver) 
-    self.driver.get(link)
-    inp_xpath_search = '//*[@id="side"]/div[1]/div/label/div/div[2]'
-    input_box_search = WebDriverWait(self.driver,20).until(lambda driver: driver.find_element_by_xpath(inp_xpath_search))
-    input_box_search.click()
-    
-    self.driver.maximize_window()
+    #Verificação da Conexão
+    self.driver = webdriver.Chrome(executable_path=self.path_chromedriver) 
+    try:
+        self.driver.get(link)
+        time.sleep(2.5)
+    except WebDriverException:
+        self.driver.quit()
+        tkinter.messagebox.showerror("ERRO", "Verificar Internet.")
+        return
+
+    #Aguardar Até QR Code ser Scaneado
+    search_xpath = '//*[@id="side"]/div[1]/div/label/div/div[2]'
+    try:
+        elem = WebDriverWait(self.driver, 200).until(
+        EC.presence_of_element_located((By.XPATH, search_xpath))
+        )
+    finally:  
+        try:
+            elem.click()
+            self.driver.maximize_window() 
+        except UnboundLocalError:
+            self.driver.quit()
+            tkinter.messagebox.showerror("ERRO", "Tempo Expirado, logar novamente.")
+            return
 
     tkinter.messagebox.showinfo("Status", "QR Code Scanned")
 
