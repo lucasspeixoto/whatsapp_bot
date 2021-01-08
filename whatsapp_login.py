ca98575cab54dbdf0bcfdf0a5fe6661966a2bc79
Lucas Peixoto <lucas.sacramoni@yahoo.com>
2021-01-07 23:18:11 -0300

    Definição de driver como variável global: self.driver

--- whatsapp_login.py
+++ whatsapp_login.py
@@ -3,20 +3,19 @@
 
 from selenium import webdriver 
 from selenium.webdriver.support.ui import WebDriverWait 
-
 import tkinter.messagebox
 
 def whatsapp_login(self):
 
     link = "https://web.whatsapp.com/"
 
-    driver = webdriver.Chrome(self.path_chromedriver) 
-    driver.get(link)
+    self.driver = webdriver.Chrome(self.path_chromedriver) 
+    self.driver.get(link)
     inp_xpath_search = '//*[@id="side"]/div[1]/div/label/div/div[2]'
-    input_box_search = WebDriverWait(driver,20).until(lambda driver: driver.find_element_by_xpath(inp_xpath_search))
+    input_box_search = WebDriverWait(self.driver,20).until(lambda driver: driver.find_element_by_xpath(inp_xpath_search))
     input_box_search.click()
     
-    driver.maximize_window()
+    self.driver.maximize_window()
 
     tkinter.messagebox.showinfo("Status", "QR Code Scanned")
 
