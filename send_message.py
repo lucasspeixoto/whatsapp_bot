417baf2f2ce9da283ad069bc75523f453d6e3360
Lucas Peixoto <lucas.sacramoni@yahoo.com>
2021-01-08 19:42:28 -0300

    Inclusão verificação de login em send_message

--- send_message.py
+++ send_message.py
@@ -1,9 +1,20 @@
 #-*- coding: utf_8 -*-
 #encoding: utf-8
 
+
+
+import tkinter as tk
 import tkinter.messagebox
-from tkinter import filedialog
 import time
 
+from selenium.webdriver.remote.command import Command
+
 def send_message(self):
-    print("Enviar....")
+
+    #Verificação Se houve conexão
+    try:
+        self.driver.execute(Command.STATUS)
+        pass
+    except Exception as e:
+        tkinter.messagebox.showerror("ERRO", "Navegador Fechado, realizar login.")
+        return
