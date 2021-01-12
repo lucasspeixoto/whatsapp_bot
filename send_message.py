a8a1d324e9dac5e28987d86463abffc78ba3389a
Lucas Peixoto <lucas.sacramoni@yahoo.com>
2021-01-11 23:33:37 -0300

    Inserção da busca dos contatos, digitação ta mensagem e envio

--- send_message.py
+++ send_message.py
@@ -8,10 +8,13 @@ import tkinter.messagebox
 import time
 
 from selenium.webdriver.remote.command import Command
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys  
 
 def send_message(self):
-    #print(self.obs.get(1.0, "end-1c"))
-    '''
+    
+    
     #Verificação Se houve conexão
     try:
         self.driver.execute(Command.STATUS)
@@ -19,9 +22,10 @@ def send_message(self):
     except Exception as e:
         tkinter.messagebox.showerror("ERRO", "Navegador Fechado, realizar login.")
         return
-    '''
+    
     #Verificação se mensagem foi inserida
-    if self.obs.get(1.0, "end-1c") == '':
+    text = self.obs.get(1.0, "end-1c")
+    if text == '':
         tkinter.messagebox.showerror("ERRO","Inserir Mensagem")
         return
     else:
@@ -38,9 +42,30 @@ def send_message(self):
     self.progress_bar.step(2)
 
     for contact in contact_list:
-        print(contact)
-    
-    
+        #Selecionar Campo de pesquisa de contato
+        search_xpath ='//*[@id="side"]/div[1]/div/label/div/div[2]'
+        elem = self.driver.find_element_by_xpath(search_xpath)
+        elem.click()
+        time.sleep(2)
+
+        #Digitar contato e Clicar 'ENTER'
+        elem.send_keys(contact)
+        time.sleep(0.5)
+        ActionChains(self.driver).send_keys(Keys.ENTER).perform()
+
+        #Verificar Se há imagem/video
+        
+
+        #Verificar Se há arquivo
+
+
+        #Inserir Mensagem
+        input_xpath = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
+        found = self.driver.find_element_by_xpath(input_xpath)
+        time.sleep(0.5)
+        found.send_keys(text + Keys.ENTER)
+
+
     self.progress_bar.stop()
     tkinter.messagebox.showinfo("Finalizado" ,"Envio Finalizado")
     return
